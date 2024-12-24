333621f0719f1514febf2d4d394d96cfc72a74a8
Muqadsa <muqadsasatti81@gmail.com>
2024-12-24 10:28:54 +0500

    Updates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+import { useRef, useEffect, useState } from 'react';
+import * as faceapi from 'face-api.js';
 import './App.css';
 
 function App() {
+  const idCardRef = useRef();
+  const selfieRef = useRef();
+  const [isModelLoaded, setIsModelLoaded] = useState(false);
+
+  useEffect(() => {
+    const loadModels = async () => {
+      await faceapi.nets.ssdMobilenetv1.loadFromUri('modal/weights');
+      await faceapi.nets.faceLandmark68Net.loadFromUri('modal/weights');
+      await faceapi.nets.faceRecognitionNet.loadFromUri('modal/weights');
+      setIsModelLoaded(true); 
+    };
+
+    loadModels();
+  }, []);
+
+  const detectFaces = async () => {
+    const idCardImage = idCardRef.current;
+    const selfieImage = selfieRef.current;
+
+    if (!idCardImage || !selfieImage) return;
+
+   
+    const idCardDetection = await faceapi
+      .detectSingleFace(idCardImage, new faceapi.SsdMobilenetv1Options())
+      .withFaceLandmarks()
+      .withFaceDescriptor();
+
+    const selfieDetection = await faceapi
+      .detectSingleFace(selfieImage, new faceapi.SsdMobilenetv1Options())
+      .withFaceLandmarks()
+      .withFaceDescriptor();
+
+ 
+    if (idCardDetection && selfieDetection) {
+      const distance = faceapi.euclideanDistance(
+        idCardDetection.descriptor,
+        selfieDetection.descriptor
+      );
+
+    
+      if (distance < 0.6) { 
+        alert("Match found! Verification successful.");
+      } else {
+        alert("Faces do not match. Please try again.");
+      }
+    } else {
+      alert("Face not detected in one or both images.");
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="gallery">
+        <img ref={idCardRef} src={require('./images/selfie.jpg')} alt="ID card" height="auto" />
+      </div>
+
+      <div className="gallery">
+        <img ref={selfieRef} src={require('./images/selfie.jpg')} alt="Selfie" height="auto" />
+      </div>
+
+     
+      <button onClick={detectFaces} disabled={!isModelLoaded}>
+        {isModelLoaded ? 'Show Result' : 'Loading Models...'}
+      </button>
     </div>
   );
 }
